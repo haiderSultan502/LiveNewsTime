d8aa7ca56b38386800dabda66ca005b264d6fba9
haiderSultan502 <haiderwains246@gmail.com>
2020-12-18 17:53:59 +0500

    Initial commit

---
+++ app/src/main/java/com/example/livenewstime/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.livenewstime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+    ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        logo=findViewById(R.id.image_logo);
+
+        Animation animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        logo.startAnimation(animFadeIn);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcome_Screen=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(welcome_Screen);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
